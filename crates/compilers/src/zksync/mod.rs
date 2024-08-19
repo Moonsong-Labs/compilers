ed34e40d9ce70570f16dff0620fdb911d378c952
elfedy <federico@moonsonglabs.com>
2024-08-19 14:53:14 -0300

    Implement standard json input

--- crates/compilers/src/zksync/mod.rs
+++ crates/compilers/src/zksync/mod.rs
@@ -1,16 +1,21 @@
 use std::{
     collections::{BTreeMap, HashSet},
-    path::PathBuf,
+    path::{Path, PathBuf},
 };
 
 use alloy_primitives::hex;
 use foundry_compilers_artifacts::{zksolc::CompilerOutput, SolcLanguage};
+use foundry_compilers_core::error::SolcError;
 
 use crate::{
     buildinfo::{BuildContext, RawBuildInfo, ETHERS_FORMAT_VERSION},
     error::Result,
-    zksolc::{input::ZkSolcVersionedInput, ZkSolcCompiler},
-    CompilerInput, Project, Source,
+    resolver::parse::SolData,
+    zksolc::{
+        input::{StandardJsonCompilerInput, ZkSolcVersionedInput},
+        ZkSolcCompiler, ZkSolcSettings,
+    },
+    CompilerInput, Graph, Project, Source,
 };
 
 use md5::Digest;
@@ -36,6 +41,87 @@ where
 {
     let sources = Source::read_all(files)?;
     self::compile::project::ProjectCompiler::with_sources(project, sources)?.compile()
+}
+
+pub fn project_standard_json_input(
+    project: &Project<ZkSolcCompiler, ZkArtifactOutput>,
+    target: &Path,
+) -> Result<StandardJsonCompilerInput> {
+    tracing::debug!(?target, "standard_json_input for zksync");
+    let graph = Graph::<SolData>::resolve(&project.paths)?;
+    let target_index = graph
+        .files()
+        .get(target)
+        .ok_or_else(|| SolcError::msg(format!("cannot resolve file at {:?}", target.display())))?;
+
+    let mut sources = Vec::new();
+    let mut unique_paths = HashSet::new();
+    let (path, source) = graph.node(*target_index).unpack();
+    unique_paths.insert(path.clone());
+    sources.push((path, source));
+    sources.extend(
+        graph
+            .all_imported_nodes(*target_index)
+            .map(|index| graph.node(index).unpack())
+            .filter(|(p, _)| unique_paths.insert(p.to_path_buf())),
+    );
+
+    let root = project.root();
+    let sources = sources
+        .into_iter()
+        .map(|(path, source)| (rebase_path(root, path), source.clone()))
+        .collect();
+
+    let mut settings: ZkSolcSettings = project.settings.clone();
+    // strip the path to the project root from all remappings
+    settings.remappings = project
+        .paths
+        .remappings
+        .clone()
+        .into_iter()
+        .map(|r| r.into_relative(project.root()).to_relative_remapping())
+        .collect::<Vec<_>>();
+
+    settings.libraries.libs = settings
+        .libraries
+        .libs
+        .into_iter()
+        .map(|(f, libs)| (f.strip_prefix(project.root()).unwrap_or(&f).to_path_buf(), libs))
+        .collect();
+
+    let input = StandardJsonCompilerInput::new(sources, settings);
+
+    Ok(input)
+}
+
+// Copied from compilers/lib private method
+fn rebase_path(base: &Path, path: &Path) -> PathBuf {
+    use path_slash::PathExt;
+
+    let mut base_components = base.components();
+    let mut path_components = path.components();
+
+    let mut new_path = PathBuf::new();
+
+    while let Some(path_component) = path_components.next() {
+        let base_component = base_components.next();
+
+        if Some(path_component) != base_component {
+            if base_component.is_some() {
+                new_path.extend(
+                    std::iter::repeat(std::path::Component::ParentDir)
+                        .take(base_components.count() + 1),
+                );
+            }
+
+            new_path.push(path_component);
+            new_path.extend(path_components);
+
+            break;
+        }
+    }
+
+    new_path.to_slash_lossy().into_owned().into()
 }
 
 pub fn build_context_new(
