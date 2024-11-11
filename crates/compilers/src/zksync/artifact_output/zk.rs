4f9527f5104c4ed5d5e67cb53404fd054962d416
Karrq <franci.dainese@gmail.com>
2024-11-11 15:09:16 +0100

    fix(artifact:zk): properly convert bytecode type (#41)
    * fix(artifact:zk): properly convert bytecode type
    
    * fix(artifact:zk): ensure correct deserialization
    
    * chore: comment formatting
    
    Co-authored-by: Nisheeth Barthwal <nbaztec@gmail.com>
    
    ---------
    
    Co-authored-by: Nisheeth Barthwal <nbaztec@gmail.com>

--- crates/compilers/src/zksync/artifact_output/zk.rs
+++ crates/compilers/src/zksync/artifact_output/zk.rs
@@ -95,8 +95,8 @@ impl From<ZkContractArtifact> for CompactContract {
     fn from(c: ZkContractArtifact) -> Self {
         // TODO: c.abi might have None, we need to get this field from solc_metadata
         Self {
-            bin: c.bytecode.clone().map(|b| b.object),
-            bin_runtime: c.bytecode.clone().map(|b| b.object),
+            bin: c.bytecode.clone().map(|b| b.object()),
+            bin_runtime: c.bytecode.clone().map(|b| b.object()),
             abi: c.abi,
         }
     }
@@ -135,6 +135,7 @@ impl ZkArtifactOutput {
         contract: Contract,
         source_file: Option<&SourceFile>,
     ) -> ZkContractArtifact {
+        let is_unlinked = contract.is_unlinked();
         let Contract {
             abi,
             metadata,
@@ -148,9 +149,11 @@ impl ZkArtifactOutput {
             missing_libraries,
         } = contract;
 
-        let (bytecode, assembly) =
-            eravm.map(|eravm| (eravm.bytecode, eravm.assembly)).unwrap_or_else(|| (None, None));
-        let bytecode = bytecode.map(|object| ZkArtifactBytecode { object, missing_libraries });
+        let (bytecode, assembly) = eravm
+            .map(|eravm| (eravm.bytecode(is_unlinked), eravm.assembly))
+            .unwrap_or_else(|| (None, None));
+        let bytecode = bytecode
+            .map(|object| ZkArtifactBytecode::with_object(object, is_unlinked, missing_libraries));
 
         ZkContractArtifact {
             abi,
