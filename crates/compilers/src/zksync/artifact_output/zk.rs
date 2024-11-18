592a571977f2b3f3653ddd0c11be34ff008d959b
Francesco Dainese <franci.dainese@gmail.com>
2024-11-18 19:06:34 +0100

    feat(compiler:zk): keep `evm` deserialization
    feat(compiler:zk): fallback to `evm` output

--- crates/compilers/src/zksync/artifact_output/zk.rs
+++ crates/compilers/src/zksync/artifact_output/zk.rs
@@ -143,6 +143,7 @@ impl ZkArtifactOutput {
             devdoc,
             storage_layout,
             eravm,
+            evm,
             ir_optimized,
             hash,
             factory_dependencies,
@@ -151,6 +152,7 @@ impl ZkArtifactOutput {
 
         let (bytecode, assembly) = eravm
             .map(|eravm| (eravm.bytecode(is_unlinked), eravm.assembly))
+            .or_else(|| evm.map(|evm| (evm.bytecode.map(|bc| bc.object), evm.assembly)))
             .unwrap_or_else(|| (None, None));
         let bytecode = bytecode
             .map(|object| ZkArtifactBytecode::with_object(object, is_unlinked, missing_libraries));
